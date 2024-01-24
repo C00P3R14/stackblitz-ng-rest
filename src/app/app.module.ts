6e2b65905a65a232b2e1227cdee1e5005bab181f
C00P3R14 <ngubane.linda9@gmail.com>
2024-01-24 15:41:16 +0200

    config CLI to stop generating standalone components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule
