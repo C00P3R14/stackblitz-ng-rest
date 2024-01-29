c8bd674e1b8162b170bed8e65c72132666d3f6e6
C00P3R14 <ngubane.linda9@gmail.com>
2024-01-29 11:53:49 +0200

    declared activatedRoute and ReqresService

--- src/app/components/user/user-detail.component.ts
+++ src/app/components/user/user-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ReqresService } from '../../services/reqres.service';
+import { User } from '../../user';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './user-detail.component.css'
 })
 export class UserDetailComponent {
-
+  user: User = {
+    id: 0,
+    first_name: '',
+    last_name: '',
+    avatar: ''
+  }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private reqresService: ReqresService
+  ) {}
 }
